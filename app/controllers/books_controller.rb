4e3dc9e7d85836ed34317432f8ecd3b7de7fe10f
vagrant <vagrant@localhost.localdomain>
2020-05-23 00:06:18 +0000

    before create tabletag

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,10 @@
 class BooksController < ApplicationController
   def index
+  	# 一覧用
+  	@books = Book.all
+
+  	# 新規登録用
+  	@book = Book.new
   end
 
   def show
@@ -9,5 +14,17 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	@book.save
+  	redirect_to books_path
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :book)
   end
 end
